d35f34700c93259b87a34eb2ff269653513b7962
Lucas Martins <80967565@telefonicati.onmicrosoft.com>
2025-02-25 11:37:19 -0300

    day one use html

---
+++ Day1/js/script.js
@@ -0,0 +1,31 @@
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+function exibirTextoNaTela(texto, tag) {
+    let resultadoDiv = document.getElementById(tag);
+    if (resultadoDiv) {
+        resultadoDiv.innerHTML = texto;
+    } else {
+        console.log(`Elemento com id '${tag}' não encontrado.`);
+    }
+}
+
+function comparar(val1, val2, nome1, nome2, tag) {
+    const resultado = val1 === parseInt(val2)
+        ? typeof val1 === typeof val2
+            ? `As variáveis ${nome1} e ${nome2} têm o mesmo valor e tipo.`
+            : `As variáveis ${nome1} e ${nome2} têm o mesmo valor, mas tipos diferentes.`
+        : `As variáveis ${nome1} e ${nome2} não têm o mesmo valor.`;
+
+    console.log(resultado);
+
+    exibirTextoNaTela(resultado, tag);
+}
+
+comparar(numeroUm, stringUm, "numeroUm", "stringUm", "li-1");
+comparar(numeroTrinta, stringTrinta, "numeroTrinta", "stringTrinta", "li-2");
+comparar(numeroDez, stringDez, "numeroDez", "stringDez", "li-3");
